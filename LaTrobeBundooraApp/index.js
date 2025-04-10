b460ad661e0e9a4d395130e8b729e89c68e9cd73
Derek Gray <113070566+derekgray91@users.noreply.github.com>
2025-04-10 14:32:07 +1000

    rejigged some directory locations

--- LaTrobeBundooraApp/index.js
+++ LaTrobeBundooraApp/index.js
@@ -4,7 +4,7 @@
 
 // index.js
 import { AppRegistry } from 'react-native';
+import App from './android/app/src/App.js'; // Ensure the path points to your new App.js file
 import { name as appName } from './app.json';
-import App from './src/App'; // Ensure the path points to your new App.js file
 
 AppRegistry.registerComponent(appName, () => App);
